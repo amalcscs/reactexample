91844eb831ad1b823893e3d8a5de908058980a13
amal c s <amalcs0006@gmail.com>
2022-02-07 18:08:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import react from 'react';
+import Home from './Home';
+import Demo from './Demo'
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <h1>welcome</h1>
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  var name="arun"
+ var age = 16
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>               
+      <h1>react app</h1>
+      <p>welcome {name}</p><br />
+      <p>your age is {age} {age >= 18?"eligible":"not eligible"} for voting</p>
+      <Demo name="teacher"/>
+      <Home text="props working" test="second value of props"/>
+      <Demo name="student"/>
+     
+      
+    
+       
+
+     
+      </div> 
+  
+      
+    
   );
 }
 
